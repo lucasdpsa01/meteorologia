e5775d8acf3317675409b3ef4f5815c408af59fe
lucas.dpsa01 <lucasdsa2801@gmail.com>
2025-03-20 23:32:36 -0300

    brasilia modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyles";
 import { theme } from "./styles/theme";
 import CurrentWeather from "./components/CurrentWeather";
 import WeatherForecast from "./components/WeatherForecast";
 
-const CurrentWeatherData = {
-  location: 'São Paulo',
-  temperature: 13,
+const App = () => {
+  const [currentWeatherData, setCurrentWeatherData] = useState(null);
+  const [forecastData, setForecastData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const API_KEY = "102d7e9ac0e16e5947c59659f075f323";
+  const city = 'Brasília';
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const CurrentWeatherResponse = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+        );
+        const CurrentWeatherJson = await CurrentWeatherResponse.json();
+        setCurrentWeatherData({
+          location: CurrentWeatherJson.name,
+          temperature: CurrentWeatherJson.main.temp,
+        });
+
+        const forecastResponse = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`
+        );
+        const forecastJson = await forecastResponse.json();
+
+        const dailyForecast = forecastJson.list
+          .filter((reading) => reading.dt_txt.includes("12:00:00"))
+          .slice(0, 5)
+          .map((forecast) => ({
+            date: forecast.dt_txt.split(' ')[0],
+            minTemp: forecast.main.temp_min,
+            maxTemp: forecast.main.temp_max,
+          }));
+
+        setForecastData(dailyForecast);
+      } catch (error) {
+        console.error("Erro ao buscar os dados da API: ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchWeatherData();
+  }, []);
+
+  if (loading) {
+    return <div>Carregando dados....</div>;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div style={{ padding: '20px', maxWidth: '800px', margin: '0 auto' }}>
+        {currentWeatherData && (
+          <CurrentWeather
+            location={currentWeatherData.location}
+            temperature={currentWeatherData.temperature}
+          />
+        )}
+        {forecastData.length > 0 && <WeatherForecast forecast={forecastData} />}
+      </div>
+    </ThemeProvider>
+  );
 };
-
-const forecastData = [
-  { date: '01/05', minTemp: 18, maxTemp: 28 },
-  { date: '02/05', minTemp: 19, maxTemp: 24 },
-  { date: '03/05', minTemp: 15, maxTemp: 26 },
-  { date: '04/05', minTemp: 20, maxTemp: 23 },
-  { date: '05/05', minTemp: 18, maxTemp: 24 },
-  { date: '06/05', minTemp: 16, maxTemp: 23 },
-  { date: '07/05', minTemp: 14, maxTemp: 22 },
-  { date: '08/05', minTemp: 18, maxTemp: 21 },
-  { date: '09/05', minTemp: 11, maxTemp: 20 },
-  { date: '10/05', minTemp: 14, maxTemp: 25 },
-  { date: '11/05', minTemp: 14, maxTemp: 23 },
-]
-
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <div style={{ padding: '20px', maxWidth: '800px', margin: '0 auto' }}>
-      <CurrentWeather 
-        location={CurrentWeatherData.location}
-        temperature={CurrentWeatherData.temperature}
-      />
-      <WeatherForecast forecast={forecastData}/>
-    </div>
-  </ThemeProvider>
-);
 
 export default App;
